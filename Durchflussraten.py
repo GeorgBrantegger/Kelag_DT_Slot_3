d73bc99f4974ad0ca8ddd5748f9430f49c736656
Brantegger Georg <Georg.brantegger@kelag.at>
2022-06-13 15:04:56 +0200

    now actually added unapplicable Hagen Poiseuille

--- Durchflussraten.py
+++ Durchflussraten.py
@@ -2,13 +2,11 @@ import numpy as np
 from math import pi 
 
 
-def Hagen_Poiseuille(dP,dx,r,vis):
+def Hagen_Poiseuille(P_above,P_below,dx,constants=[1,1]):
+    dP      = P_above-P_below
+    r       = constants[0]
+    vis     = constants[1]    
     Q = (pi*r**4)/(8*vis)*dP/dx
     return Q
-
-
-def get_Q_from_dP(P_above,P_below,dx,constants=[1,1]):
-    r       = constants[0]
-    vis     = constants[1]
 
 
