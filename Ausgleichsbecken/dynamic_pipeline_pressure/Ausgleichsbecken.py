f774b2e52d4943829835fcbf9c2ddb94bfc0732e
Georg ´Brantegger <georg.brantegger@gmail.com>
2022-07-01 08:58:29 +0200

    Clean up before combining pipeline and reservoir code

--- Ausgleichsbecken/dynamic_pipeline_pressure/Ausgleichsbecken.py
+++ Ausgleichsbecken/dynamic_pipeline_pressure/Ausgleichsbecken.py
@@ -1,31 +1,5 @@
 import numpy as np 
 
-def Volume_trend(influx, outflux, timestep=1, V_0=0):
-    '''
-    Returns the trend and the volume and the final volume,  defined
-    by influx and outflux patterns. The optional parameter timestep
-    defines the time increment over which the fluxes are changing.
-    '''
-    net_flux = influx-outflux
-    delta_V = net_flux*timestep
-    V_trend = V_0+np.cumsum(delta_V)
-    V_end = V_trend[-1]
-    return V_end,  V_trend
-
-def Height_trend(V_trend, area=1, h_crit_low=-np.inf, h_crit_high=np.inf):
-    '''
-    Returns the trend and the height and the final height,  defined
-    by influx and outflux patterns as well as the crosssection area.
-    The optional parameters h_crit_low/high indicate limits that the height
-    should never exceed. If this occures,  TRUE is returned in the corresponding
-    h_crit_flag.
-    '''
-    h_trend = V_trend/area
-    h_crit_flag_low = np.any(h_trend <= h_crit_low)
-    h_crit_flag_high = np.any(h_trend >= h_crit_high)
-    h_end = h_trend[-1]
-    return h_trend, h_end, h_crit_flag_low, h_crit_flag_high
-
 def get_h_halfstep(initial_height, influx, outflux, timestep, area):
     h0      = initial_height
     Q_in    = influx
@@ -45,7 +19,6 @@ def FODE_function(x, h, alpha, p, rho=1000., g=9.81):
 
 def e_RK_4(yn, h, dt, Q0, Q1, A0, A1, p0, p1):
     alpha = (A1/A0-1)
-
     h_hs = get_h_halfstep(h, Q0, Q1, dt, A0)
     p_hs = get_p_halfstep(p0, p1)
     Y1 = yn
@@ -55,13 +28,3 @@ def e_RK_4(yn, h, dt, Q0, Q1, A0, A1, p0, p1):
     ynp1 = yn + dt/6*(FODE_function(Y1, h, alpha, p)+2*FODE_function(Y2, h_hs, alpha, p_hs)+ \
         2*FODE_function(Y3, h_hs, alpha, p_hs)+ FODE_function(Y4, h, alpha, p))
 
-
-
-
-## testing
-# if __name__ == "__main__":
-#     influx = np.full([1, 100],  6)
-#     outflux = np.full_like(influx,  4)
-#     V_end,  V_trend = Volume_trend(influx,  outflux, timestep=0.5, V_0 = 100)
-#     print(V_end)
-#     print(V_trend)
--- Messing Around/pressure_propagation.py
+++
@@ -1,2 +0,0 @@
-def pressure_update(p,p1=-1,p2=-1):
-    return 1/4*(2*p+p1+p2)
