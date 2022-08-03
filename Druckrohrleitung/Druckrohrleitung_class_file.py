fix for numerical runaway of rounding errors due to turbine-pipeline interatction via a convergence method in the turbine and a "damping" trick on the reservoir velocity plus: code cleanup with consistent naming of variables

Druckrohrleitung/Druckrohrleitung_class_file.py
@@ -1,5 +1,13 @@
 import numpy as np
 
+#importing pressure conversion function
+import sys
+import os
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+from functions.pressure_conversion import pressure_conversion
+
 class Druckrohrleitung_class:
 # units
     acceleration_unit           = r'$\mathrm{m}/\mathrm{s}^2$'
@@ -13,72 +21,66 @@ class Druckrohrleitung_class:
     velocity_unit               = r'$\mathrm{m}/\mathrm{s}$' # for flux and pressure propagation
     volume_unit                 = r'$\mathrm{m}^3$'
 
-    acceleration_unit_print     = 'm/s²'
-    angle_unit_print            = 'rad'
-    area_unit_print             = 'm²'
-    density_unit_print          = 'kg/m³'
-    flux_unit_print             = 'm³/s'
-    length_unit_print           = 'm'
-    time_unit_print             = 's'
-    velocity_unit_print         = 'm/s' # for flux and pressure propagation
-    volume_unit_print           = 'm³'
+    acceleration_unit_disp      = 'm/s²'
+    angle_unit_disp             = 'rad'
+    area_unit_disp              = 'm²'
+    density_unit_disp           = 'kg/m³'
+    flux_unit_disp              = 'm³/s'
+    length_unit_disp            = 'm'
+    time_unit_disp              = 's'
+    velocity_unit_disp          = 'm/s' # for flux and pressure propagation
+    volume_unit_disp            = 'm³'
+
+    g = 9.81
 
 # init
-    def __init__(self,total_length,diameter,number_segments,pipeline_angle,Darcy_friction_factor,rho=1000,g=9.81):
+    def __init__(self,total_length,diameter,number_segments,pipeline_angle,Darcy_friction_factor,pw_vel,timestep,pressure_unit_disp,rho=1000):
         self.length     = total_length  	                            # total length of the pipeline
         self.dia        = diameter                                      # diameter of the pipeline
         self.n_seg      = number_segments                               # number of segments for the method of characteristics
         self.angle      = pipeline_angle                                # angle of the pipeline
-        self.f_D        = Darcy_friction_factor                         # = Rohrreibungszahl oder flow coefficient        
+        self.f_D        = Darcy_friction_factor                         # = Rohrreibungszahl oder flow coefficient  
+        self.c          = pw_vel
+        self.dt         = timestep
         self.density    = rho                                           # density of the liquid in the pipeline
-        self.g          = g                                             # gravitational acceleration
         
         self.A          = (diameter/2)**2*np.pi
 
         self.dx         = total_length/number_segments                  # length of each segment
-        self.l_vec      = np.arange(0,(number_segments+1),1)*self.dx    # vector giving the distance from each node to the start of the pipeline
+        self.x_vec      = np.arange(0,(number_segments+1),1)*self.dx    # vector giving the distance from each node to the start of the pipeline
 
-        # initialize for get_info method
-        self.c          = '--'
-        self.dt         = '--'
+        self.pressure_unit_disp = pressure_unit_disp
 
 # setter
-    def set_pressure_propagation_velocity(self,c):
-        self.c      = c         # propagation velocity of the pressure wave
-        self.dt     = self.dx/c # timestep derived from c, demanded by the method of characteristics
-
-    def set_number_of_timesteps(self,number_timesteps):
-        self.nt     = number_timesteps  # number of timesteps 
-        if self.c == '--':
-            raise Exception('Please set the pressure propagation velocity before setting the number of timesteps.')
-        else:
-            self.t_vec = np.arange(0,self.nt*self.dt,self.dt)
-
     def set_initial_pressure(self,pressure):
         # initialize the pressure distribution in the pipeline
         if np.size(pressure)   == 1:
-            self.p0 = np.full_like(self.l_vec,pressure)
-        elif np.size(pressure) == np.size(self.l_vec):
-            self.p0 = pressure
+            p0 = np.full_like(self.x_vec,pressure)
+        elif np.size(pressure) == np.size(self.x_vec):
+            p0 = pressure
         else:
-             raise Exception('Unable to assign initial pressure. Input has to be of size 1 or' + np.size(self.l_vec))
+             raise Exception('Unable to assign initial pressure. Input has to be of size 1 or' + np.size(self.x_vec))
         
         #initialize the vectors in which the old and new pressures are stored for the method of characteristics
-        self.p_old  = self.p0.copy()
-        self.p      = self.p0.copy()
+        self.p_old  = p0.copy()
+        self.p      = p0.copy()
+        self.p_min  = p0.copy()
+        self.p_max  = p0.copy()
     
     def set_initial_flow_velocity(self,velocity):
         # initialize the velocity distribution in the pipeline
         if np.size(velocity)   == 1:
-            self.v0 = np.full_like(self.l_vec,velocity)
-        elif np.size(velocity) == np.size(self.l_vec):
-            self.v0 = velocity
+            v0 = np.full_like(self.x_vec,velocity)
+        elif np.size(velocity) == np.size(self.x_vec):
+            v0 = velocity
         else:
-             raise Exception('Unable to assign initial velocity. Input has to be of size 1 or' + np.size(self.l_vec))    
+             raise Exception('Unable to assign initial velocity. Input has to be of size 1 or' + np.size(self.x_vec))    
 
         #initialize the vectors in which the old and new velocities are stored for the method of characteristics
-        self.v_old  = self.v0.copy()
-        self.v      = self.v0.copy()
+        self.v_old  = v0.copy()
+        self.v      = v0.copy()
+        self.v_min  = v0.copy()
+        self.v_max  = v0.copy()
     
     def set_boundary_conditions_next_timestep(self,p_reservoir,v_turbine):
         # derived from the method of characteristics, one can set the boundary conditions for the pressures and flow velocities at the reservoir and the turbine
@@ -112,16 +114,16 @@ class Druckrohrleitung_class:
         self.p[0]           = p_boundary_res
         self.p[-1]          = p_boundary_tur
 
-    def set_steady_state(self,ss_flux,ss_level_reservoir,area_reservoir,pl_vec,h_vec):
+    def set_steady_state(self,ss_flux,ss_level_reservoir,area_reservoir,x_vec,h_vec):
         # set the pressure and velocity distributions, that allow a constant flow of water from the (steady-state) reservoir to the (steady-state) turbine
             # the flow velocity is given by the constant flow through the pipe
-        ss_v0 = np.full(self.n_seg+1,ss_flux/self.A)
+        ss_v0 = np.full_like(self.x_vec,ss_flux/self.A)
 
         # the static pressure is given by static state pressure of the reservoir, corrected for the hydraulic head of the pipe and friction losses
         ss_v_in_res     = abs(ss_flux/area_reservoir)
         ss_v_out_res    = abs(ss_flux/self.A)
         ss_pressure_res = self.density*self.g*(ss_level_reservoir)+self.density*ss_v_out_res*(ss_v_in_res-ss_v_out_res)
-        ss_pressure     = ss_pressure_res+(self.density*self.g*h_vec)-(self.f_D*pl_vec/self.dia*self.density/2*ss_v0**2)
+        ss_pressure     = ss_pressure_res+(self.density*self.g*h_vec)-(self.f_D*x_vec/self.dia*self.density/2*ss_v0**2)
 
         self.set_initial_flow_velocity(ss_v0)
         self.set_initial_pressure(ss_pressure)
@@ -135,29 +137,60 @@ class Druckrohrleitung_class:
         # :<10 pads the self.value to be 10 characters wide
         print_str = (f"The pipeline has the following attributes: {new_line}" 
             f"----------------------------- {new_line}"
-            f"Length                =       {self.length:<10} {self.length_unit_print} {new_line}"
-            f"Diameter              =       {self.dia:<10} {self.length_unit_print} {new_line}"
+            f"Length                =       {self.length:<10} {self.length_unit_disp} {new_line}"
+            f"Diameter              =       {self.dia:<10} {self.length_unit_disp} {new_line}"
             f"Number of segments    =       {self.n_seg:<10} {new_line}"
             f"Number of nodes       =       {self.n_seg+1:<10} {new_line}"
-            f"Length per segments   =       {self.dx:<10} {self.length_unit_print} {new_line}"
-            f"Pipeline angle        =       {round(self.angle,3):<10} {self.angle_unit_print} {new_line}"
+            f"Length per segments   =       {self.dx:<10} {self.length_unit_disp} {new_line}"
+            f"Pipeline angle        =       {round(self.angle,3):<10} {self.angle_unit_disp} {new_line}"
             f"Pipeline angle        =       {angle_deg}° {new_line}"
             f"Darcy friction factor =       {self.f_D:<10} {new_line}"
-            f"Density of liquid     =       {self.density:<10} {self.density_unit_print} {new_line}"
-            f"Pressure wave vel.    =       {self.c:<10} {self.velocity_unit_print} {new_line}"
-            f"Simulation timestep   =       {self.dt:<10} {self.time_unit_print} {new_line}"
+            f"Density of liquid     =       {self.density:<10} {self.density_unit_disp} {new_line}"
+            f"Pressure wave vel.    =       {self.c:<10} {self.velocity_unit_disp} {new_line}"
+            f"Simulation timestep   =       {self.dt:<10} {self.time_unit_disp} {new_line}"
             f"Number of timesteps   =       {self.nt:<10} {new_line}"
-            f"Total simulation time =       {self.nt*self.dt:<10} {self.time_unit_print} {new_line}"
+            f"Total simulation time =       {self.nt*self.dt:<10} {self.time_unit_disp} {new_line}"
             f"----------------------------- {new_line}"
             f"Velocity and pressure distribution are vectors and are accessible by the .v and .p attribute of the pipeline object")
 
         print(print_str)    
 
-    def get_current_pressure_distribution(self):
-        return self.p
+    def get_current_pressure_distribution(self,disp=False):
+        if disp == True:
+            return pressure_conversion(self.p,self.pressure_unit,self.pressure_unit_disp)
+        elif disp == False:
+            return self.p
 
     def get_current_velocity_distribution(self):
         return self.v
+
+    def get_current_flux_distribution(self):
+        return self.v*self.A
+
+    def get_lowest_pressure_per_node(self,disp=False):
+        if disp == True:
+            return pressure_conversion(self.p_min,self.pressure_unit,self.pressure_unit_disp)
+        elif disp == False:
+            return self.p_min
+
+    def get_highest_pressure_per_node(self,disp=False):
+        if disp == True:
+            return pressure_conversion(self.p_max,self.pressure_unit,self.pressure_unit_disp)
+        elif disp == False:
+            return self.p_max
+
+    def get_lowest_velocity_per_node(self):
+        return self.v_min
+
+    def get_highest_velocity_per_node(self):
+        return self.v_max
+
+    def get_lowest_flux_per_node(self):
+        return self.v_min*self.A
+
+    def get_highest_flux_per_node(self):
+        return self.v_max*self.A
+
 
     def timestep_characteristic_method(self):
     # use the method of characteristics to calculate the pressure and velocities at all nodes except the boundary ones
@@ -179,6 +212,12 @@ class Druckrohrleitung_class:
 
             self.p[i] = 0.5*(self.p_old[i+1]+self.p_old[i-1]) - 0.5*rho*c*(self.v_old[i+1]-self.v_old[i-1]) \
                 +f_D*rho*c*dt/(4*D)*(abs(self.v_old[i+1])*self.v_old[i+1]-abs(self.v_old[i-1])*self.v_old[i-1])
+
+        # update overall min and max values for pressure and velocity per node
+        self.p_min = np.minimum(self.p_min,self.p)
+        self.p_max = np.maximum(self.p_max,self.p)
+        self.v_min = np.minimum(self.v_min,self.v)
+        self.v_max = np.maximum(self.v_max,self.v)
 
         # prepare for next call
             # use .copy() to write data to another memory location and avoid the usual python reference pointer

Regler/Regler_class_file.py
@@ -84,17 +84,17 @@ class PI_controller_class:
         # use a list to be able to append more easily - will get converted to np.array when needed
         self.error_history = [0]            
 
-        self.control_variable = -99
-
         self.cv_lower_limit = lower_limit   # limits for the controll variable
         self.cv_upper_limit = upper_limit   # limits for the controll variable
 
 # setter
+
+    
     def set_setpoint(self,setpoint):
         self.SP = setpoint
 
     def set_control_variable(self,control_variable, display_warning=True):
-        if display_warning == True and self.control_variable != -99:
+        if display_warning == True:
             print('WARNING! You are setting the control variable of the PI controller manually \
                 and are not using the .update_controll_variable() method')
         self.control_variable = control_variable

Turbinen/Turbinen_class_file.py
@@ -1,8 +1,10 @@
-from time import time
 import numpy as np
+
 #importing pressure conversion function
 import sys
 import os
+
+from pyparsing import alphanums
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
@@ -11,7 +13,7 @@ from functions.pressure_conversion import pressure_conversion
 class Francis_Turbine:
  # units 
     # make sure that units and print units are the same
-    # units are used to label graphs and print units are used to have a bearable format when using pythons print()
+    # units are used to label graphs and disp units are used to have a bearable format when using pythons print()
     density_unit        = r'$\mathrm{kg}/\mathrm{m}^3$'
     flux_unit           = r'$\mathrm{m}^3/\mathrm{s}$'
     LA_unit             = '%'
@@ -20,30 +22,28 @@ class Francis_Turbine:
     velocity_unit       = r'$\mathrm{m}/\mathrm{s}$'
     volume_unit         = r'$\mathrm{m}^3$'
 
-    density_unit_print      = 'kg/m³'
-    flux_unit_print         = 'm³/s'
-    LA_unit_print           = '%'
-    pressure_unit_print     = 'mWS' 
-    time_unit_print         = 's'
-    velocity_unit_print     = 'm/s'
-    volume_unit_print       = 'm³'
+    density_unit_disp   = 'kg/m³'
+    flux_unit_disp      = 'm³/s'
+    LA_unit_disp        = '%'
+    time_unit_disp      = 's'
+    velocity_unit_disp  = 'm/s'
+    volume_unit_disp    = 'm³'
 
     g = 9.81    # m/s²  gravitational acceleration
 
  # init
-    def __init__(self, Q_nenn,p_nenn,t_closing=-1.,timestep=-1.):
+    def __init__(self, Q_nenn,p_nenn,t_closing,timestep,pressure_unit_disp):
         self.Q_n    = Q_nenn                                    # nominal flux 
         self.p_n    = p_nenn                                    # nominal pressure
         self.LA_n   = 1. # 100%                                 # nominal Leitapparatöffnung
-        h           = pressure_conversion(p_nenn,'Pa','MWs')    # nominal pressure in terms of hydraulic head
-        self.A      = Q_nenn/(np.sqrt(2*self.g*h)*0.98)         # Ersatzfläche
-
         self.dt     = timestep                                  # simulation timestep
-        self.t_c = t_closing                                    # closing time
+        self.t_c    = t_closing                                 # closing time
         self.d_LA_max_dt = 1/t_closing                          # maximal change of LA per second
 
+        self.pressure_unit_disp = pressure_unit_disp
+
         # initialize for get_info() - parameters will be converted to display -1 if not overwritten
-        self.p  = pressure_conversion(-1,self.pressure_unit_print,self.pressure_unit)
+        self.p  = pressure_conversion(-1,self.pressure_unit_disp,self.pressure_unit)
         self.Q  = -1.
         self.LA = -0.01 
 
@@ -54,18 +54,21 @@ class Francis_Turbine:
         self.LA = LA
         # warn user, that the .set_LA() method should not be used ot set LA manually
         if display_warning == True:
-            print('Consider using the .update_LA() method instead of setting LA manually')
-
-    def set_timestep(self,timestep,display_warning=True):
-        # set Leitapparatöffnung
-        self.dt = time
-        # warn user, that the .set_LA() method should not be used ot set LA manually
-        if display_warning == True:
-            print('WARNING: You are changing the timestep of the turbine simulation. This has implications on the simulated closing speed!')
+            print('You are setting the guide vane opening of the turbine manually. \n \
+                This is not an intended use of this method. \n \
+                Refer to the .update_LA() method instead.')
 
     def set_pressure(self,pressure):
         # set pressure in front of the turbine
         self.p = pressure
+
+    def set_steady_state(self,ss_flux,ss_pressure):
+        # calculate and set steady state LA, that allows the flow of ss_flux at ss_pressure through the
+            # turbine at the steady state LA
+        ss_LA = self.LA_n*ss_flux/self.Q_n*np.sqrt(self.p_n/ss_pressure)
+        if ss_LA < 0 or ss_LA > 1:
+            raise Exception('LA out of range [0;1]')
+        self.set_LA(ss_LA,display_warning=False)
 
 #getter
     def get_current_Q(self):
@@ -80,10 +83,13 @@ class Francis_Turbine:
     def get_current_LA(self):
         return self.LA
 
+    def get_current_pressure(self):
+        return pressure_conversion(self.p,self.pressure_unit,self.pressure_unit_disp)
+
     def get_info(self, full = False):
         new_line = '\n'
-        p   = pressure_conversion(self.p,self.pressure_unit,self.pressure_unit_print)
-        p_n = pressure_conversion(self.p_n,self.pressure_unit,self.pressure_unit_print)
+        p   = pressure_conversion(self.p,self.pressure_unit,self.pressure_unit_disp)
+        p_n = pressure_conversion(self.p_n,self.pressure_unit,self.pressure_unit_disp)
 
         
         if full == True:
@@ -91,33 +97,34 @@ class Francis_Turbine:
             print_str = (f"Turbine has the following attributes: {new_line}" 
                 f"----------------------------- {new_line}"
                 f"Type                  =       Francis {new_line}"
-                f"Nominal flux          =       {self.Q_n:<10} {self.flux_unit_print} {new_line}"
-                f"Nominal pressure      =       {round(p_n,3):<10} {self.pressure_unit_print}{new_line}"
-                f"Nominal LA            =       {self.LA_n*100:<10} {self.LA_unit_print} {new_line}"
-                f"Closing time          =       {self.t_c:<10} {self.time_unit_print} {new_line}"
-                f"Current flux          =       {self.Q:<10} {self.flux_unit_print} {new_line}" 
-                f"Current pipe pressure =       {round(p,3):<10} {self.pressure_unit_print} {new_line}"
-                f"Current LA            =       {self.LA*100:<10} {self.LA_unit_print} {new_line}"
-                f"Simulation timestep   =       {self.dt:<10} {self.time_unit_print} {new_line}"
+                f"Nominal flux          =       {self.Q_n:<10} {self.flux_unit_disp} {new_line}"
+                f"Nominal pressure      =       {round(p_n,3):<10} {self.pressure_unit_disp}{new_line}"
+                f"Nominal LA            =       {self.LA_n*100:<10} {self.LA_unit_disp} {new_line}"
+                f"Closing time          =       {self.t_c:<10} {self.time_unit_disp} {new_line}"
+                f"Current flux          =       {self.Q:<10} {self.flux_unit_disp} {new_line}" 
+                f"Current pipe pressure =       {round(p,3):<10} {self.pressure_unit_disp} {new_line}"
+                f"Current LA            =       {self.LA*100:<10} {self.LA_unit_disp} {new_line}"
+                f"Simulation timestep   =       {self.dt:<10} {self.time_unit_disp} {new_line}"
                 f"----------------------------- {new_line}")
         else:
             # :<10 pads the self.value to be 10 characters wide
             print_str = (f"The current attributes are: {new_line}" 
                 f"----------------------------- {new_line}"
-                f"Current flux          =       {self.Q:<10} {self.flux_unit_print} {new_line}" 
-                f"Current pipe pressure =       {round(p,3):<10} {self.pressure_unit_print} {new_line}"
-                f"Current LA            =       {self.LA*100:<10} {self.LA_unit_print} {new_line}"
+                f"Current flux          =       {self.Q:<10} {self.flux_unit_disp} {new_line}" 
+                f"Current pipe pressure =       {round(p,3):<10} {self.pressure_unit_disp} {new_line}"
+                f"Current LA            =       {self.LA*100:<10} {self.LA_unit_disp} {new_line}"
                 f"----------------------------- {new_line}")
 
         print(print_str)
 
-# methods
+# update methods
     def update_LA(self,LA_soll):
         # update the Leitappartöffnung and consider the restrictions of the closing time of the turbine
-        LA_diff = self.LA-LA_soll                   # calculate the difference to the target LA
-        LA_diff_max = self.d_LA_max_dt*self.dt     # calculate the maximum change in LA based on the given timestep  
+        LA_diff = self.LA-LA_soll                                           # calculate the difference to the target LA
+        LA_diff_max = self.d_LA_max_dt*self.dt                              # calculate the maximum possible change in LA based on the given timestep  
         LA_diff = np.sign(LA_diff)*np.min(np.abs([LA_diff,LA_diff_max]))    # calulate the correct change in LA
         
+        # make sure that the LA is not out of the range [0;1]
         LA_new = self.LA-LA_diff
         if LA_new < 0.:
             LA_new = 0.
@@ -125,10 +132,42 @@ class Francis_Turbine:
             LA_new = 1.
         self.set_LA(LA_new,display_warning=False)
 
-    def set_steady_state(self,ss_flux,ss_pressure):
-        # calculate and set steady state LA, that allows the flow of ss_flux at ss_pressure through the
-            # turbine at the steady state LA
-        ss_LA = self.LA_n*ss_flux/self.Q_n*np.sqrt(self.p_n/ss_pressure)
-        if ss_LA < 0 or ss_LA > 1:
-            raise Exception('LA out of range [0;1]')
-        self.set_LA(ss_LA,display_warning=False)
+# methods
+    def converge(self,convergence_parameters):
+        # small numerical disturbances (~1e-12 m/s) in the velocity can get amplified at the turbine node, because the new velocity of the turbine and the
+        # new pressure from the forward characteristic are not compatible.
+        eps                 = 1e-12                                         # convergence criterion: iteration change < eps
+        iteration_change    = 1.                                            # change in Q from one iteration to the next
+        i                   = 0                                             # safety variable. break loop if it exceeds 1e6 iterations
+        g                   = self.g                                        # gravitational acceleration
+        p                   = convergence_parameters[0]                     # pressure at second to last node (see method of characterisctics - boundary condidtions)
+        v                   = convergence_parameters[1]                     # velocity at second to last node (see method of characterisctics - boundary condidtions)
+        D                   = convergence_parameters[2]                     # diameter of the pipeline
+        area_pipe           = convergence_parameters[3]                     # area of the pipeline
+        alpha               = convergence_parameters[4]                     # elevation angle of the pipeline
+        f_D                 = convergence_parameters[5]                     # Darcy friction coefficient
+        c                   = convergence_parameters[6]                     # pressure wave propagtation velocity
+        rho                 = convergence_parameters[7]                     # density of the liquid
+        dt                  = convergence_parameters[8]                     # timestep of the characteristic method
+        
+        p_old               = self.get_current_pressure()                   
+        Q_old               = self.get_current_Q()                          
+        v_old               = Q_old/area_pipe                               
+
+
+        while iteration_change > eps:
+            self.set_pressure(p_old)
+            Q_new = self.get_current_Q()
+            v_new = Q_new/area_pipe
+            p_new = p-rho*c*(v_old-v)+rho*c*dt*g*np.sin(alpha)-f_D*rho*c*dt/(2*D)*abs(v)*v
+
+            iteration_change = abs(Q_old-Q_new)
+            Q_old = Q_new.copy()
+            p_old = p_new.copy()
+            v_old = v_new.copy()
+            i = i+1
+            if i == 1e6:
+                print('did not converge')
+                break
+        # print(i)
+        self.Q = Q_new